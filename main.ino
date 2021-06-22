701b75ebb168b76b4e8429cef1bbc7ac11878457
mickbos <mickbos@hotmail.com>
2021-06-22 20:29:20 +0200

    Initial commit

---
+++ main.ino
@@ -0,0 +1,26 @@
+int ThermistorPin = A0;
+int Vo;
+float R1 = 10000; // value of R1 on board
+float logR2, R2, T;
+float c1 = 0.001129148, c2 = 0.000234125, c3 = 0.0000000876741; //steinhart-hart coeficients for thermistor
+
+void setup() {
+  Serial.begin(9600);
+}
+
+float readTemp(int thermistorPin){
+  Vo = analogRead(ThermistorPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0); //calculate resistance on thermistor
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); // temperature in Kelvin
+  return T - 273.15; //convert Kelvin to Celcius
+}
+
+void loop() {
+  T = readTemp(ThermistorPin);
+  Serial.print("Temperature: "); 
+  Serial.print(T);
+  Serial.println(" C"); 
+
+  delay(500);
+}
